f1d3b17048ef42a70a8fe98fb8cab95289acffd1
WinterSeasonn <jasonha567@gmail.com>
2026-02-07 01:43:58 -0500

    the start

---
+++ Spring-boot/winter/src/main/java/com/social_backend/winter/WinterApplication.java
@@ -0,0 +1,14 @@
+package com.social_backend.winter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinterApplication {
+
+	public static void main(String[] args) {
+		System.out.println("check");
+		SpringApplication.run(WinterApplication.class, args);
+	}
+
+}
---
+++ Spring-boot/winter/src/test/java/com/social_backend/winter/WinterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social_backend.winter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
