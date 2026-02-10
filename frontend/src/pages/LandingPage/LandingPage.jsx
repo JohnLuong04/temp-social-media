e778838993ca8cb52be090e6b1befa88bb441761
John Luong <scorpionf904@gmail.com>
2026-02-09 19:04:59 -0500

    Built skeleton for Log in page (Minimal styling).

---
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,74 @@
+import "./LandingPage.css"
+import {Link, useNavigate} from "react-router-dom";
+import {useState} from "react";
+
+export default function LandingPage(){
+    const navigate = useNavigate();
+
+    const [userData, setUserData] = useState({
+        username: "",
+        password: ""
+    });
+
+    function handleChange(e){
+        setUserData(prev => ({
+            ...prev,
+            [e.target.name]: e.target.value
+        }));
+    }
+
+    async function handleSubmit(e){
+        e.preventDefault();
+        // Do something for backend, delete console log
+        console.log(userData);
+        // should authenticate if user exists before navigating to home page
+        // for now just checking if anything has been entered
+        if(userData.username && userData.password){
+            navigate("/home");
+        }
+    }
+
+    // Replace h1 with actual app name
+    // replace img with our own logo or something else
+    return (
+        <main className="landing-page">
+            <section className="landing-page-left">
+                <h1 className={"app-name-landing-page"}>Temp Social Media Name</h1>
+                <img
+                    className={"app-logo-landing-page"}
+                    src={"/src/assets/react.svg"}
+                    alt={"App Logo"}
+                />
+            </section>
+
+            <section className="landing-page-right">
+                <form className={"login-form"} onSubmit={handleSubmit}>
+                    <input
+                        type={"text"}
+                        placeholder={"Username"}
+                        aria-label={"Username field"}
+                        name={"username"}
+                        value={userData.username}
+                        onChange={handleChange}
+                        required={true}
+                    />
+                    <input
+                        type={"password"}
+                        placeholder={"Password"}
+                        aria-label={"Password field"}
+                        name={"password"}
+                        value={userData.password}
+                        onChange={handleChange}
+                        required={true}
+                    />
+                    <button className={"login-button"}>Log in</button>
+                </form>
+
+                <p className={"sign-up-prompt"}>
+                    Don't have an account?
+                    <Link to={"/sign-up"} className={"sign-up-link"}> Create Account</Link>
+                </p>
+            </section>
+        </main>
+    )
+}
---
+++ frontend/src/pages/Signup/Signup.jsx
@@ -0,0 +1,10 @@
+import {Link, useNavigate} from "react-router-dom";
+import {useState} from "react";
+
+export default function SignupPage(){
+    return (
+        <form className="signup-form">
+            <h1>Signup Here</h1>
+        </form>
+    )
+}
