cc034c7ca9d50882ea1f53dcde6a234b2e7a2dcd
WinterSeasonn <jasonha567@gmail.com>
2026-02-14 15:54:44 -0500

    Profile post, edit, get and delete functions

--- Spring-boot/src/main/java/com/social_backend/Persistence/Profileback.java
+++ Spring-boot/src/main/java/com/social_backend/Persistence/Profileback.java
@@ -12,27 +12,61 @@ public class Profileback {
 
     private static String db_location = "jdbc:sqlite:spring-boot/src/main/resources/social-media-database.db";
 
-    /*
-        All usernames in UserData are unique
-        Each profile is linked by a user from the username
-
+    /**
+     * Deleting a profile 
+     * @param username user_id where we deleting
      */
-    // public boolean post_profile(String username, String nickname) throws SQLException {
-    //     try(Connection con = DriverManager.getConnection(db_location)){
-    //         Statement stmt = con.createStatement();
-    //         stmt.execute("INSERT INTO profile(username,)")
+    public void delete_profile(String username){
+        try(Connection con = DriverManager.getConnection(db_location)){
+            String sql_remove = "DELETE FROM PROFILE WHERE user_id = ?";
+            PreparedStatement statement = con.prepareStatement(sql_remove);
+            statement.setString(1, username);
+            statement.executeUpdate();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
 
-    //     }
-    // }
+    /**
+     * Post a profile data into tha profile the table
+     * @param username Unique Username
+     * @param nickname Nickname can be anything
+     */
+    public void post_profile(String username, String nickname) {
+        try(Connection con = DriverManager.getConnection(db_location)){
+            String sql_post = "INSERT INTO profile(user_id, nickname) VALUES(?,?)";
+            PreparedStatement statement = con.prepareStatement(sql_post);
+            statement.setString(1, username);
+            statement.setString(2, nickname);
+            statement.executeUpdate();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Edit the profile nickname should never edit the username
+     * @param username unique username
+     * @param new_nickname user new nickname // Cannot be Null 
+     */
+    public void edit_profile(String username, String new_nickname){
+        try(Connection con = DriverManager.getConnection(db_location)){
+            String sql = "UPDATE Profile SET nickname = ? WHERE user_id = ?";
+            PreparedStatement statement = con.prepareStatement(sql);
+            statement.setString(1, new_nickname);
+            statement.setString(2, username);
+            statement.executeUpdate();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
 
 
-    /*
-        Get_Profile gets the profile from the database from username. Where username
-        is a unique name from UserData table. 
-        @Param
-        Username: the Unique Username 
-        @Return
-        Profile [Unique Username, Nickname]
+    /**
+     *  Get_Profile gets the profile from the database from username. Where username
+     *  is a unique name from UserData table. 
+     *  @Param Username: the Unique Username 
+     *  @Return Profile [Unique Username, Nickname]
     */
     public String[] get_profile(String username){
         try(Connection con = DriverManager.getConnection(db_location)){
@@ -57,7 +91,10 @@ public class Profileback {
     public static void main(String[] args){
 
         Profileback test = new Profileback();
+        test.post_profile("winter", "test2");
         System.out.println(Arrays.toString(test.get_profile("WINTER")));
+        System.out.println(Arrays.toString(test.get_profile("winter")));
+        test.delete_profile("winter");
         System.out.println(Arrays.toString(test.get_profile("winter")));
 
     }
